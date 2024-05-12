20bb5be40068b1332309cbd6c7ea7a254e2f6c8a
DYNAMIC <99006135+mohit162001@users.noreply.github.com>
2024-05-12 16:49:50 +0530

    Update Item.jsx

--- src/Component/Item/Item.jsx
+++ src/Component/Item/Item.jsx
@@ -6,7 +6,7 @@ const Item = (props) => {
   const {setMenu,theme} = useContext(ShopContext)
   return (
     <div className={theme==='dark-theme'?"item dark":"item"}>
-        <Link onClick={()=>setMenu(props.category?props.category:'')} to={`/product/${props.id}`} ><img onClick={window.scrollTo(0,0)} src={"https://clothy-backend-app.vercel.app/media/media/"+props.image} alt={props.name} /></Link>
+        <Link onClick={()=>setMenu(props.category?props.category:'')} to={`/product/${props.id}`} ><img onClick={window.scrollTo(0,0)} src={"https://clothy-backend-app.vercel.app/media/"+props.image} alt={props.name} /></Link>
         <p>{props.name}</p>
         <div className="item-prices">
           <div className={theme==='dark-theme'?"item-price-new dark":"item-price-new"}>
